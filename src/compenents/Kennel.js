476a8265f6dffbed3298a28fcfeff4d49e100b91
keyanoush <keyanoush@Keyanoush.localdomain>
2021-06-04 14:52:07 -0500

    user authentication implementaion

--- src/compenents/Kennel.js
+++ src/compenents/Kennel.js
@@ -1,27 +1,41 @@
-import React from "react"
-import "./Kennel.css"
-//import { Animal } from "./animal/Animal"
-//import { Employee } from "./employee/Employee"
-//import { Customer } from "./customer/Customer"
-//import { Location } from "./location/Location"
-//import { AnimalProvider } from "./animal/AnimalProvider"
-//import { CustomerProvider } from "./customer/CustomerProvider"
-//import { EmployeeProvider } from "./employee/EmployeeProvider"
-//import { LocationProvider } from "./location/LocationProvider"
-import { NavBar } from "./nav/NavBar"
-import { ApplicationViews } from "./ApplicationView"
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationView";
+import { NavBar } from "./nav/NavBar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import "./Kennel.css";
 
 export const Kennel = () => (
-    <>
-        <NavBar />
-        
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-        
-        <ApplicationViews />
-    </>
-)
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("kennel_customer")) {
+          return (
+            <>
+              <NavBar />
+
+              <h2>Nashville Kennels</h2>
+              <small>Loving care when you're not there.</small>
+              <address>
+                <div>Visit Us at the Nashville North Location</div>
+                <div>500 Puppy Way</div>
+              </address>
+
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+);
